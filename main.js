f0046cae9a1aa80af79d42b514724a69b77f1b6f
obamamen <Moosmegens@gmail.com>
2024-12-18 13:39:59 +0100

    More games
    more games and added delay to loading looks better

--- main.js
+++ main.js
@@ -16,10 +16,13 @@ games.push(new game("Solitaire", "https://www.officialgamerules.org/card-games/s
 games.push(new game("Pesten", "https://www.spel-regels.nl/pesten-html/",["2","3", "4", "5+"], "ja"));
 games.push(new game("Klaverjassen", "https://www.spel-regels.nl/klaverjassen/",["4"], "nee"));
 games.push(new game("Poker", "https://nl.wikipedia.org/wiki/Poker",["2","3", "4", "5+"], "nee"));
-games.push(new game("Black jack", "https://www.spelregels.eu/wp-content/uploads/2021/01/spelregels-blackjack.pdf",["2","3", "4", "5+"], "ja"));
+games.push(new game("Black jack", "https://www.spelregels.eu/wp-content/uploads/2021/01/spelregels-blackjack.pdf",["2","3", "4", "5+"], "nee"));
+games.push(new game("Toepen", "https://nl.wikipedia.org/wiki/Toepen#:~:text=Een%20speler%20die%20zijn%20was,toont%20deze%20aan%20alle%20deelnemers.",["2","3", "4", "5+"], "ja"));
+games.push(new game("Oorlogje", "https://nl.wikipedia.org/wiki/Oorlogje_(kaartspel)",["3","4","5+"], "ja"));
 
 
 console.log(JSON.stringify(games, null, 5));
+
 
 function recommendGame() {
     const players = document.getElementById('players').value;
@@ -36,31 +39,28 @@ function recommendGame() {
         )
     });
 
-    filteredGames.forEach(game => {
-        const p = document.createElement("a");
-        p.textContent  = game.name;
-        p.href = game.link;
-        p.target = '_blank';
-        gameContainer.appendChild(p);
-    })
+    setTimeout(() => {
+        filteredGames.forEach(game => {
+            const p = document.createElement("a");
+            p.textContent  = game.name;
+            p.href = game.link;
+            p.target = '_blank';
+            gameContainer.appendChild(p);
+        })
+        const randomIndex = Math.floor(Math.random() * filteredGames.length);
 
-    const randomIndex = Math.floor(Math.random() * filteredGames.length);
-
-    console.log(filteredGames.length);
-    if (filteredGames.length === 0) {
-        const p = document.createElement("p");
-        p.textContent  = "Geen spellen, probeer opnieuw!";
-        gameContainer.appendChild(p);
-    } else {
-        const p = document.createElement("a");
-        p.target = '_blank';
-        p.href = filteredGames[randomIndex].link;
-        p.textContent  = filteredGames[randomIndex].name;
-        //gameContainer.appendChild(p);
-    }
-
-    //gameText.textContent = "Chess\nPoker";
-
-
-    result.style.display = 'block';
+        console.log(filteredGames.length);
+        if (filteredGames.length === 0) {
+            const p = document.createElement("p");
+            p.textContent  = "Geen spellen, probeer opnieuw!";
+            gameContainer.appendChild(p);
+        } else {
+            const p = document.createElement("a");
+            p.target = '_blank';
+            p.href = filteredGames[randomIndex].link;
+            p.textContent  = filteredGames[randomIndex].name;
+            //gameContainer.appendChild(p);
+        }
+        result.style.display = 'block';
+    }, 100);
 }
